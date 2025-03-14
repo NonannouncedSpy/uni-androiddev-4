7ca2d9fecd48c63d5d330d10f086f3f543577de4
Ygg <gex_klo@mail.ru>
2025-03-14 13:36:50 +0300

    Add Discipline class
    Will be used to store the disciplines the user registered for.

---
+++ app/src/main/java/com/ygg/uni_androiddev_4/Discipline.java
@@ -0,0 +1,28 @@
+package com.ygg.uni_androiddev_4;
+
+public class Discipline {
+    private String name;
+    private String lessonDay;
+    private String time;
+    private String teacher;
+
+    public Discipline(String name, String lessonDay, String time, String teacher) {
+        this.name = name;
+        this.lessonDay = name;
+        this.time = name;
+        this.teacher = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getLessonDay() {
+        return lessonDay;
+    }
+    public String getTime() {
+        return time;
+    }
+    public String getTeacher() {
+        return teacher;
+    }
+}
