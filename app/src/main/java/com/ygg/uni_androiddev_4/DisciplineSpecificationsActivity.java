33b7fa302f72547fffd8cf2bf2101fec5d0a94db
Ygg <gex_klo@mail.ru>
2025-03-14 11:59:58 +0300

    Embark to Glory and Horizon
    or 'initial commit' if you wanna be boring
    3 activities

---
+++ app/src/main/java/com/ygg/uni_androiddev_4/DisciplineSpecificationsActivity.java
@@ -0,0 +1,14 @@
+package com.ygg.uni_androiddev_4;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DisciplineSpecificationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discipline_specifications);
+    }
+}
