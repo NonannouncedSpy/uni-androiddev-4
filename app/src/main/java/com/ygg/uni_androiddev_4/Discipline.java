40a1c96cca9be24c6f34d904240331bf01308e8b
Ygg <gex_klo@mail.ru>
2025-03-15 07:54:08 +0300

    Make Discipline parcelable

--- app/src/main/java/com/ygg/uni_androiddev_4/Discipline.java
+++ app/src/main/java/com/ygg/uni_androiddev_4/Discipline.java
@@ -1,6 +1,11 @@
 package com.ygg.uni_androiddev_4;
 
-public class Discipline {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+public class Discipline implements Parcelable {
     private String name;
     private String lessonDay;
     private String time;
@@ -13,6 +18,16 @@ public class Discipline {
         this.time = time;
         this.teacher = teacher;
         this.comment = comment;
+    }
+    public Discipline(String name, String lessonDay, String time, String teacher) {
+        this(name, lessonDay, time, teacher, "None");
+    }
+    public Discipline(Parcel source) {
+        name = source.readString();
+        lessonDay = source.readString();
+        time = source.readString();
+        teacher = source.readString();
+        comment = source.readString();
     }
 
     public String getName() {
@@ -30,4 +45,31 @@ public class Discipline {
     public String getComment() {
         return comment;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(lessonDay);
+        dest.writeString(time);
+        dest.writeString(teacher);
+        dest.writeString(comment);
+    }
+
+    public static final Parcelable.Creator<Discipline> CREATOR = new Parcelable.Creator<Discipline>() {
+
+        @Override
+        public Discipline createFromParcel(Parcel source) {
+            return new Discipline(source);
+        }
+
+        @Override
+        public Discipline[] newArray(int size) {
+            return new Discipline[size];
+        }
+    };
 }
